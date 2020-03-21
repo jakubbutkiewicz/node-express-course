72619800121eb8a5b9888b5826736efe8d7187b6
Jakub.Butkiewicz <jakub.butkiewicz@acaisoft.com>
2020-03-21 21:12:58 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const mockData = [{ name: 'Mark' }, { name: 'Jill' }];
 
@@ -21,6 +22,26 @@ app.get('/user/:id', (req, res) => {
   });
 });
 
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = 'billyTheKid';
+  const mockPassword = 'superSecret';
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: 'password and username match!',
+      token: 'encrypted token goes here'
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'password and username do not match'
+    });
+  }
+});
 app.listen(8000, () => {
   console.log('server is running');
 });
