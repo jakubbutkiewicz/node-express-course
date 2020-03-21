fe7d2c19ec502b7e4766d5e70d0651d634f59752
Jakub.Butkiewicz <jakub.butkiewicz@acaisoft.com>
2020-03-21 20:53:56 +0100

    add first GET route

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const mockData = [{ name: 'Mark' }, { name: 'Jill' }];
+
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'successfully got users. Nice!',
+    users: mockData
+  });
+});
+
 app.listen(8000, () => {
   console.log('server is running');
 });
